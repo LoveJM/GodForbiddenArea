5c88cb857de9e8f08df547ab7eeddd33e729c3c1
dingding <dingding2002820@sina.com>
2015-01-25 16:26:49 +0800

    2.2.1例2-2
    在程序中调用Notepad.exe文件,启动停止和观察Notepad进程

---
+++ MyNetProgramProj/2.2.1StartStopProcess_example2_2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _2._2._1StartStopProcess_example2_2
+{
+    partial class frm_StartStopProcess
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.btnStartProcess = new System.Windows.Forms.Button();
+            this.btnEndProcess = new System.Windows.Forms.Button();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(1, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(762, 215);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnStartProcess
+            // 
+            this.btnStartProcess.Location = new System.Drawing.Point(188, 351);
+            this.btnStartProcess.Name = "btnStartProcess";
+            this.btnStartProcess.Size = new System.Drawing.Size(124, 33);
+            this.btnStartProcess.TabIndex = 1;
+            this.btnStartProcess.Text = "启动进程";
+            this.btnStartProcess.UseVisualStyleBackColor = true;
+            this.btnStartProcess.Click += new System.EventHandler(this.btnStartProcess_Click);
+            // 
+            // btnEndProcess
+            // 
+            this.btnEndProcess.Location = new System.Drawing.Point(395, 351);
+            this.btnEndProcess.Name = "btnEndProcess";
+            this.btnEndProcess.Size = new System.Drawing.Size(124, 33);
+            this.btnEndProcess.TabIndex = 2;
+            this.btnEndProcess.Text = "终止进程";
+            this.btnEndProcess.UseVisualStyleBackColor = true;
+            this.btnEndProcess.Click += new System.EventHandler(this.btnEndProcess_Click);
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.Location = new System.Drawing.Point(613, 351);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(78, 33);
+            this.btn_Refresh.TabIndex = 3;
+            this.btn_Refresh.Text = "刷新";
+            this.btn_Refresh.UseVisualStyleBackColor = true;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // frm_StartStopProcess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 406);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.btnEndProcess);
+            this.Controls.Add(this.btnStartProcess);
+            this.Controls.Add(this.listView1);
+            this.Name = "frm_StartStopProcess";
+            this.Text = "启动和终止进程";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button btnStartProcess;
+        private System.Windows.Forms.Button btnEndProcess;
+        private System.Windows.Forms.Button btn_Refresh;
+    }
+}
+
---
+++ MyNetProgramProj/2.2.1StartStopProcess_example2_2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace _2._2._1StartStopProcess_example2_2
+{
+    public partial class frm_StartStopProcess : Form
+    {
+        int fileIndex;
+        string fileName = "Notepad.exe";
+        Process process1 = new Process();
+        public frm_StartStopProcess()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;//以详细列表方式展示
+            //参数：列名称,宽度(像素),水平对其方式
+            listView1.Columns.Add("进程ID", 70, HorizontalAlignment.Left);
+            listView1.Columns.Add("进程名称", 70, HorizontalAlignment.Left);
+            listView1.Columns.Add("占用内存", 70, HorizontalAlignment.Left);
+            listView1.Columns.Add("启动时间", 70, HorizontalAlignment.Left);
+            listView1.Columns.Add("文件名", 280, HorizontalAlignment.Left);
+        }
+
+        private void btnStartProcess_Click(object sender, EventArgs e)
+        {
+            string argument = Application.StartupPath + "\\myfile" + fileIndex + ".txt";
+            if (File.Exists(argument) == false)//文件不存在则创建一个 
+            {
+                File.CreateText(argument);
+            }
+            //设置要启动的应用程序名称及参数
+            ProcessStartInfo ps = new ProcessStartInfo(fileName, argument);
+            ps.WindowStyle = ProcessWindowStyle.Normal;//启动的应用程序模式为正常窗口
+            fileIndex++;
+            Process p = new Process();
+            p.StartInfo = ps;//指定启动信息
+            p.Start();//启动进程
+            //等待启动完成，否则获取进程信息可能失败
+            p.WaitForInputIdle();
+            RefreshListView();
+        }
+
+        private void btnEndProcess_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;//光标为等待的样式
+            //创建Process组件数组,并将它们与指定进程名称(Notepad)的所有进程资源相关联
+            Process[] myProcess;
+            myProcess = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(fileName));
+            foreach (Process p in myProcess) 
+            {
+                //通过向进程主窗口发送关闭消息达到关闭进程的目的
+                p.CloseMainWindow();
+                //等待1000毫秒
+                Thread.Sleep(1000);
+                //释放与此组件相关联的所有资源
+                p.Close();
+            }
+            fileIndex = 0;
+            RefreshListView();
+            this.Cursor = Cursors.Default;//终止进程后,光标转为默认光标
+        }
+        private void RefreshListView() 
+        {
+            listView1.Items.Clear();
+            //创建process数据,并将它们与系统内所有进程相关联
+            Process[] processes = Process.GetProcessesByName(Path.GetFileNameWithoutExtension(fileName));
+            foreach (Process p in processes) 
+            {
+                //将每个进程的进程名称，占用的物理内存及启动时间加入到listview中
+                ListViewItem item = new ListViewItem(
+                    new string[]{
+                    p.Id.ToString(),//进程ID
+                    p.ProcessName,//进程名称
+                    string.Format("{0}kb",p.WorkingSet64/1024f),//进程占用的物理内存
+                    string.Format("{0}",p.StartTime),//进程的启动时间
+                    p.MainModule.FileName//调用的完整文件名
+                    }
+                    );
+                listView1.Items.Add(item);
+            }
+
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            RefreshListView();
+        }
+        
+    }
+}
